b1139e63d35133b12db42d501d55a88e3cd74c4a
laveshpanwar <heyitslaveshpanwar@gmail.com>
2026-06-20 16:49:35 +0530

    Create 01_variables_datatypes.py

---
+++ 01_variables_datatypes.py
@@ -0,0 +1,23 @@
+name = "lavesh panwar"
+age = 17
+price = 28.17
+
+print (name)
+print ("my name is : " , name )
+print ("my age is : " , age )
+print ("my price is : " , price )
+print (type(name))
+print (type(age))
+print(type(price))
+name1="kanika"
+name2='''kanika'''
+name3='kanika'
+print(name1)
+print(name2)
+print(name3)
+old=True
+young=False
+a=None
+print(type(old))
+print(type(young))
+print(type(a))
