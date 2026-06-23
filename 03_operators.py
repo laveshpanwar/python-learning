44ee60840c536402a88b73ac914a402afac9dfa5
laveshpanwar <heyitslaveshpanwar@gmail.com>
2026-06-23 19:33:25 +0530

    Create 03_operators.py

---
+++ 03_operators.py
@@ -0,0 +1,95 @@
+# arithmetic operators
+a=6
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #remainder
+print(a**b) #a^b 
+
+
+# relational/comparison operators
+a=50
+b=20
+
+print(a==b) #False
+print(a!=b) #True
+print(a<b) #False
+print(a>b) #True
+print(a>=b) #True
+print(a<=b) #False
+
+# assignment operators
+
+
+num=10
+num+=6
+print("num:",num)
+num=10
+num-=20   #num=num+10 
+print("num:",num)
+num=10
+num*=6
+print("num:",num)
+num=10
+num/=2
+print("num:",num)
+num=10
+num%=2
+print("num:",num)
+num=10
+num**=2
+print(num)
+print("num:",num)
+ 
+
+
+# logical operators
+
+print(not True)
+print(not False)
+
+
+a=10
+b=2
+print(not True)
+print(not (a<b))
+print(not (a>b))
+
+#and operator
+val1=True
+val2=False
+print(val1 and val2)
+
+val1=True
+val2=True
+print(val1 and val2)
+ 
+val1=False
+val2=True
+print(val1 and val2)
+
+val1=False
+val2=False
+print(val1 and val2)
+
+# or operator
+
+val1=True
+val2=True
+print(val1 or val2)
+
+val1=True
+val2=False
+print(val1 or val2)
+
+val1=False
+val2=False
+print(val1 or val2)
+
+
+val1=False
+val2=True
+print(val1 or val2)
