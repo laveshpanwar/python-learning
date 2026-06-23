1aac0d03a3c860bdc1ac9a952e72003b1759cfde
laveshpanwar <heyitslaveshpanwar@gmail.com>
2026-06-23 19:35:28 +0530

    Create 05_inputpython.py

---
+++ 05_inputpython.py
@@ -0,0 +1,11 @@
+#input in pyhton 
+name=input("enter you name:")
+print("welcome",name)
+
+name=input("enter name:")
+age=input("enter age: ")
+marks=input("enter marks:")
+
+print("welcome",name)
+print("entered into",age )
+print("total marks",marks)
