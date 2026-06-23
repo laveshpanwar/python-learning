b18c41a3c3707e9d8d119273174e7a07ea4db10c
laveshpanwar <heyitslaveshpanwar@gmail.com>
2026-06-23 19:34:41 +0530

    Create 04_typeconversions&casting.py

---
+++ 04_typeconversions&casting.py
@@ -0,0 +1,23 @@
+#type conversion
+
+a=10   #int
+b=2.5   #float
+sum=a+b    #int->float convert
+print(type(sum))
+
+a="2"
+b=4.23
+print(type(a))
+#give error bcz string cant be added into float if we add a+b
+
+#type casting
+a=int("2")
+b=1.2
+sun=a+b
+print(sum)
+print(type(sum))
+
+a=3.14  #float
+a=str(a)
+print(type(a))
+
